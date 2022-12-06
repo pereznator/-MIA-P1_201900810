ce33e4b87cf628b4327a8dbe48a493b32ed4a1f2
jorigot <jorgeperez@inlandlogistics.co>
2022-12-06 10:37:40 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string verPalabras(string entrada);
+
+int main() {
+  string entrada;
+  cout << "Ingrese la entrada" << endl;
+  getline(cin, entrada);
+  cout << "La entrada es:" << entrada;
+  return 0;
+}
+
+string verPalabras(string entrada) {
+  
+}
